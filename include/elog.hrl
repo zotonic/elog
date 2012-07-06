24d830243f2f41e9531364eed9cf7449904a939e
hejin <hejin1026@gmail.com>
2012-07-06 15:29:14 +0800

    add ~n

--- include/elog.hrl
+++ include/elog.hrl
@@ -5,16 +5,14 @@
 %%% Created : 31 Mar 2009
 %%% License : http://www.opengoss.com/
 %%%
-%%% Copyright (C) 2007-2009, www.opengoss.com 
+%%% Copyright (C) 2007-2009, www.opengoss.com
 %%%----------------------------------------------------------------------
 
 %% ---------------------------------
 %% Logging mechanism
+
 -define(PRINT(Format, Args),
     io:format(Format, Args)).
-
--define(PRINT_MSG(Format),
-    io:format(Format)).
 
 -define(DEBUG(Format, Args),
     lager:debug(Format, Args)).
@@ -26,37 +24,37 @@
     lager:debug(Msg)).
 
 -define(INFO(Format, Args),
-    lager:info(Format, Args)).
+    lager:info(Format ++ "~n~n", Args)).
 
 -define(INFO_TRACE(Dest, Format, Args),
-    lager:info(Dest, Format, Args)).
+    lager:info(Dest, Format ++ "~n~n", Args)).
 
 -define(INFO_MSG(Msg),
     lager:info(Msg)).
-			      
+
 -define(WARNING(Format, Args),
-    lager:warning(Format, Args)).
+    lager:warning(Format ++ "~n~n", Args)).
 
 -define(WARNING_TRACE(Dest, Format, Args),
-    lager:warning(Dest, Format, Args)).
+    lager:warning(Dest, Format ++ "~n~n", Args)).
 
 -define(WARNING_MSG(Msg),
     lager:warning(Msg)).
-			      
+
 -define(ERROR(Format, Args),
-    lager:error(Format, Args)).
+    lager:error(Format ++ "~n~n", Args)).
 
 -define(ERROR_TRACE(Dest, Format, Args),
-    lager:error(Dest, Format, Args)).
+    lager:error(Dest, Format ++ "~n~n", Args)).
 
 -define(ERROR_MSG(Msg),
     lager:error(Msg)).
 
 -define(CRITICAL(Format, Args),
-    lager:critical(Format, Args)).
+    lager:critical(Format ++ "~n~n", Args)).
 
 -define(CRITICAL_TRACE(Dest, Format, Args),
-    lager:critical(Dest, Format, Args)).
+    lager:critical(Dest, Format ++ "~n~n", Args)).
 
 -define(CRITICAL_MSG(Msg),
     lager:critical(Msg)).
